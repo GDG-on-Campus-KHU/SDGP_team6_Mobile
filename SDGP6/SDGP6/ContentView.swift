62fa976157dfa4954cd1c957b52396c0161fc464
Gardeniaa101 <onejwon33@naver.com>
2025-04-09 23:18:57 +0900

    [Setting] #1 - 프로젝트 초기 설정

---
+++ SDGP6/SDGP6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDGP6
+//
+//  Created by HanJW on 4/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
